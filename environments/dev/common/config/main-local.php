5c78e7fff625e9212231bc2fca268878d11c1896
Lisergide <lisergide@gmail.com>
2019-05-16 00:57:21 +0300

    edit environments/dev/common/config/main-local.php

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -4,7 +4,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost:3307;dbname=yii2-adv',
+            'dsn' => 'mysql:host=localhost:3306;dbname=yii2-adv',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
